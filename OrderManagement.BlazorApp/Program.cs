af5d717f82f5ad034feacbae818714aed6cb85ba
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-03-19 03:49:00 +0600

    show orders in grid

--- OrderManagement.BlazorApp/Program.cs
+++ OrderManagement.BlazorApp/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OrderManagement.BlazorApp;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/api/v1/") });
 
