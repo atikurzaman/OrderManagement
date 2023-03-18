9c3b45054a04ba4133843e1f478452cc7a700d66
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-03-19 04:10:38 +0600

    add validation in add order form

--- OrderManagement.BlazorApp/Models/Order.cs
+++ OrderManagement.BlazorApp/Models/Order.cs
@@ -1,11 +1,17 @@
-﻿namespace OrderManagement.BlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagement.BlazorApp.Models
 {
     public class Order
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(256, ErrorMessage = "Name must not exceed 256 characters")]
         public string Name { get; set; } = String.Empty;
 
+        [Required]
+        [StringLength(256, ErrorMessage = "Name must not exceed 256 characters")]
         public string State { get; set; } = String.Empty;
     }
 }
