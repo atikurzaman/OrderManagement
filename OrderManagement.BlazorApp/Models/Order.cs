7b9942ce7936b18228ab8a51f312aaebeeb0ded7
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-03-19 00:25:20 +0600

    add blazor projects

---
+++ OrderManagement.BlazorApp/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagement.BlazorApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string State { get; set; } = String.Empty;
+    }
+}
--- OrderManagement.WebUI/Program.cs
+++ OrderManagement.WebUI/Program.cs
@@ -11,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
 
 var app = builder.Build();
 
@@ -22,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
