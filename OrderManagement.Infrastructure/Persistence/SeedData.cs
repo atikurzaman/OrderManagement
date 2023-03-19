478b862ca5431dd4f45f651aa363afcf852e8fb7
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-03-20 03:28:55 +0600

    add seeding

--- OrderManagement.Infrastructure/Persistence/SeedData.cs
+++ OrderManagement.Infrastructure/Persistence/SeedData.cs
@@ -23,17 +23,67 @@ namespace OrderManagement.Infrastructure.Persistence
                     new Order
                     {
                         Name = "William",
-                        State = "Shakespeare"
+                        State = "Shakespeare",
+                        Windows = new List<Window>
+                        {
+                            new Window
+                            {
+                                Name = "A51",
+                                QuantityOfWindows = 4,
+                                TotalSubElements = 3,
+                                SubElements = new List<SubElement>
+                                {
+                                    new SubElement { Element = 1, Type = "Doors", Width = 1200, Height = 1850 },
+                                    new SubElement { Element = 2, Type = "Window", Width = 800, Height = 1850 },
+                                    new SubElement { Element = 3, Type = "Window", Width = 700, Height = 1850 }
+                                }
+                            },
+                            new Window
+                            {
+                                Name = "C Zone 5",
+                                QuantityOfWindows = 2,
+                                TotalSubElements = 1,
+                                SubElements = new List<SubElement>
+                                {
+                                    new SubElement { Element = 1, Type = "Window", Width = 1500, Height = 2000 }                                   
+                                }
+                            }
+                        }
                     },
                     new Order
                     {
-                        Name = "William",
-                        State = "Shakespeare"
+                        Name = "California Hotel AJK",
+                        State = "CA",
+                        Windows = new List<Window>
+                        {
+                            new Window
+                            {
+                                Name = "GLB",
+                                QuantityOfWindows = 3,
+                                TotalSubElements = 2,
+                                SubElements = new List<SubElement>
+                                {
+                                    new SubElement { Element = 1, Type = "Doors", Width = 1400, Height = 2200 },
+                                    new SubElement { Element = 2, Type = "Window", Width = 600, Height = 2200 }                                    
+                                }
+                            },
+                            new Window
+                            {
+                                Name = "C Zone 5",
+                                QuantityOfWindows = 2,
+                                TotalSubElements = 1,
+                                SubElements = new List<SubElement>
+                                {
+                                    new SubElement { Element = 1, Type = "Window", Width = 1500, Height = 2000 },
+                                    new SubElement { Element = 1, Type = "Window", Width = 1500, Height = 2000 }
+                                }
+                            }
+                        }
                     }
                 };
 
                 context.Orders.AddRange(orders);
-                context.SaveChanges();                
+                context.SaveChanges();
             }
         }
     }
